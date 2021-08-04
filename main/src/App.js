0218cbd98ffb8eb0e0f7538a6e634cf9e542e7dc
ganghyun choi <goaqjrj13@gmail.com>
2021-08-04 13:53:28 +0900

    사용자

--- main/src/App.js
+++ main/src/App.js
@@ -1,7 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "antd/dist/antd.css";
+import { Comment, Avatar, Form, Button, List, Input } from "antd";
+import moment from "moment";
 
 function App() {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
     <div>
       <div id="header-space">
@@ -13,17 +23,28 @@ function App() {
         </div>
       </div>
       <div id="body">
-        <div id="body-map">
+        <div id="post">
+          <div id="author-space">
+            <img src="logo192.png"></img>
+            <div id="author-profile">작성자</div>
+          </div>
           <div id="map">
             <img src="images\지도캡쳐본.png"></img>
           </div>
           <div id="interaction">
-            <div id="good_button">좋아요 999+</div>
-            <div id="share_button">공유 999+</div>
+            <div id="good_button">
+              <Button size="small">좋아요</Button>
+            </div>
+            <div id="share_button">
+              <Button size="small">공유</Button>
+            </div>
           </div>
           <div id="social_space">
-            <div id="post">게시물</div>
-            <div id="comment"></div>
+            <div id="main-comment">2021-08-03 날씨좋음</div>
+            <div id="comment-space">
+              <div id="read_comment">댓글들</div>
+              <div id="comment"></div>
+            </div>
           </div>
         </div>
       </div>
